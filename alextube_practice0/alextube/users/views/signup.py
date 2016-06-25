6688c8e3820e944ef6abd99cf35111ee5af05523
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-25 22:02:03 +0900

    init SignupView, update Loginview
    initialized signupview, and updated signup template for signup function.
    updated loginview and login template, for it had errors in code,

--- alextube_practice0/alextube/users/views/signup.py
+++ alextube_practice0/alextube/users/views/signup.py
@@ -1,5 +1,7 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 
 
 class SignupView(View):
@@ -11,3 +13,15 @@ class SignupView(View):
                 context={},
         )
 
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        phonenumber = request.POST.get("phonenumber")
+
+        user = get_user_model().objects.create_user(
+                username=username,
+                password=password,
+                phonenumber=phonenumber,
+        )
+
+        return redirect(reverse("login"))
