af65cc26cf919bb41683a36c7044489634deecef
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-25 15:41:17 +0900

    init static files
    categorized into css and js. separated css into partials. revised
    static files directories in settings.

--- alextube_practice0/alextube/alextube/settings/partials/base.py
+++ alextube_practice0/alextube/alextube/settings/partials/base.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'alextube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+           os.path.join(BASE_DIR, "alextube", "templates") 
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+       os.path.join(BASE_DIR, "alextube", "static"), 
+        ]
 
 # Custom User model
 AUTH_USER_MODEL = "users.Users"
