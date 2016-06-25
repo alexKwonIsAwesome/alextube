29021ccbb60456ee3fea6133bfbaacd5e7725237
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-25 22:16:24 +0900

    init logoutfunction:
    init LogoutView, conncted with /login/ url
    updated header.html for logout link when user is authenticated

--- alextube_practice0/alextube/alextube/urls.py
+++ alextube_practice0/alextube/alextube/urls.py
@@ -20,5 +20,6 @@ from users.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^signup/', SignupView.as_view(), name="signup"),
-    url(r'^login/', LoginView.as_view(),name="login"),
+    url(r'^login/', LoginView.as_view(), name="login"),
+    url(r'^logout/', LogoutView.as_view(), name="logout"),
 ]
