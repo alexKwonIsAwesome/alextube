382e15564fe40a58616196a0b6230f6ef8f5d046
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-26 11:59:24 +0900

    init message alert funciton
    init message alert template and its stylesheet as partial file,
    updated base template including partial files,
    updated logoutview by including successful logout message function

--- alextube_practice0/alextube/users/views/logout.py
+++ alextube_practice0/alextube/users/views/logout.py
@@ -1,6 +1,7 @@
 from django.views.generic import View
 from django.core.urlresolvers import reverse
 from django.contrib.auth import logout
+from django.contrib import messages
 from django.shortcuts import redirect
 
 
@@ -8,4 +9,9 @@ class LogoutView(View):
 
     def get(self, request, *args, **kwargs):
         logout(request)
+        messages.add_message(
+                request,
+                messages.SUCCESS,
+                "You are successfully logged out. Thanks for using and hope to see you again."
+        )
         return redirect(reverse("login"))
