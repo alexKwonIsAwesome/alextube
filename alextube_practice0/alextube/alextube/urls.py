91e733dfa4efe9c2695a716ef6cd1e840ef7fd24
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-26 18:55:15 +0900

    fixed urls.py:
    added explicit end keywords on users

--- alextube_practice0/alextube/alextube/urls.py
+++ alextube_practice0/alextube/alextube/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url('', include('social.apps.django_app.urls', namespace='social')),
+
     url(r'^', include("users.urls", namespace="users")),
-    url('', include('social.apps.django_app.urls', namespace='social'))
 ]
